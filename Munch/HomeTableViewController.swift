d66a3cf58e0a9236990d28524d939c9bda73becc
Adam Ginzberg <ginzberg@stanford.edu>
2015-11-20 04:57:23 -0800

    most of front end of claims

--- Munch/HomeTableViewController.swift
+++ Munch/HomeTableViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
 
+    @IBOutlet weak var coupon: UILabel!
+    @IBOutlet weak var restaurant: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var walkOrDrive: UISegmentedControl!
+    @IBOutlet weak var recommended: UIButton!
+    
+    private enum FontSizes: Int {
+        case Primary = 14
+        case Secondary = 11
+    }
+    
+    private enum FontStyles: String {
+        case Primary = "AvenirNext-Bold"
+        case Secondary = "AvenirNext-DemiBold"
+        case Tertiary = "AvenirNext-Regular"
+    }
+    
+    private struct Colors {
+        static let Green = UIColor(red: 0x40, green: 0xBA, blue: 0x91, alpha: 1)
+        static let Orange = UIColor(red: 0xFF, green: 0x99, blue: 0x00, alpha: 1)
+        static let LightGray = UIColor(red: 0xF4, green: 0xF5, blue: 0xF7, alpha: 1)
+        static let DarkGray = UIColor(red: 0x8C, green: 0x86, blue: 0x8E, alpha: 1)
+    }
+    
+    private let borderWidth = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        coupon?.text = coupon?.text!.uppercaseString
+        coupon.font = UIFont(name: FontStyles.Secondary.rawValue, size: CGFloat(FontSizes.Primary.rawValue))
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
+        restaurant.font = UIFont(name: FontStyles.Tertiary.rawValue, size: CGFloat(FontSizes.Secondary.rawValue))
+        distance.font = UIFont(name: FontStyles.Tertiary.rawValue, size: CGFloat(FontSizes.Secondary.rawValue))
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
@@ -27,15 +56,15 @@ class HomeTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 1
+//    }
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
