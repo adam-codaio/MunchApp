a439ed1bc04ce9f291b973a7fd4444f583e4019f
Adam Ginzberg <ginzberg@stanford.edu>
2015-12-01 16:45:55 -0800

    image view constraints phew

--- Munch/RestaurantTableViewController.swift
+++ Munch/RestaurantTableViewController.swift
@@ -149,12 +149,12 @@ class RestaurantTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return 0
     }
 
     /*
