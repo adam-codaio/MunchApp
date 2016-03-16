01f83a28256efb70c4f7d8183cccfda9e4dd25e1
Adam Ginzberg <ginzberg@stanford.edu>
2016-03-16 03:12:44 -0700

    im going to commit this before i fuck everything up

--- Munch/Restaurant+CoreDataProperties.swift
+++ Munch/Restaurant+CoreDataProperties.swift
@@ -2,8 +2,8 @@
 //  Restaurant+CoreDataProperties.swift
 //  Munch
 //
-//  Created by Adam Ginzberg on 12/1/15.
-//  Copyright © 2015 Stanford University. All rights reserved.
+//  Created by Adam Ginzberg on 3/16/16.
+//  Copyright © 2016 Stanford University. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -14,11 +14,12 @@ import CoreData
 
 extension Restaurant {
 
+    @NSManaged var address: String?
     @NSManaged var distance: NSNumber?
     @NSManaged var hours: String?
+    @NSManaged var id: NSNumber?
     @NSManaged var name: String?
     @NSManaged var phone_number: String?
-    @NSManaged var address: String?
     @NSManaged var promotions: NSSet?
 
 }
--- Munch/User+CoreDataProperties.swift
+++ Munch/User+CoreDataProperties.swift
@@ -2,8 +2,8 @@
 //  User+CoreDataProperties.swift
 //  Munch
 //
-//  Created by Adam Ginzberg on 12/1/15.
-//  Copyright © 2015 Stanford University. All rights reserved.
+//  Created by Adam Ginzberg on 3/16/16.
+//  Copyright © 2016 Stanford University. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
@@ -17,7 +17,5 @@ extension User {
     @NSManaged var name: String?
     @NSManaged var rating: NSNumber?
     @NSManaged var userClaims: NSSet?
-    
-    
 
 }
